823e23920e4b6781158b5d73d495497aee82dbb5
NeroBirdy <nirvanav.39@gmail.com>
2024-11-30 12:18:09 +0500

    .

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -1,46 +1,15 @@
-import asyncio
-import logging
-import sys
-from os import getenv
-
-from aiogram import Bot, Dispatcher, html, types
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart, BaseFilter
-from aiogram.types import Message, WebAppData, KeyboardButton, ReplyKeyboardMarkup
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from src.settings import settings
 from src.bot.handlers import register_routes
 
-from src.bot.filters import filters
+storage = MemoryStorage()
 
 bot = Bot(token=settings.telegram_bot.BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    # Создаем кнопку с WebApp
-    button = KeyboardButton(text="Открыть WebApp", web_app=types.WebAppInfo(url="https://d62d-188-19-202-149.ngrok-free.app"))
-
-    # Создаем клавиатуру с этой кнопкой
-    keyboard = ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True)
-    await message.answer("Нажмите кнопку ниже для авторизации:", reply_markup=keyboard)
-
-
-@dp.message(filters.WebAppDataFilter())
-async def webapp_data_handler(message: types.Message):
-    # Проверяем, содержит ли сообщение данные WebApp
-    if message.web_app_data:
-        data = message.web_app_data.data
-        await message.answer(f"Получены данные из WebApp: {data}")
-    else:
-        await message.answer("Нет данных от WebApp.")
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+register_routes(dp)
 
 async def start_bot():
     try:
--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -1,12 +1,33 @@
 from aiogram import types, Router
 from aiogram.filters import Command
 
-from src.bot.filters.filters import IsPrivateFilter
+from src.bot.filters import filters
+
+from aiogram import Bot, Dispatcher, html, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, BaseFilter
+from aiogram.types import Message, WebAppData, KeyboardButton, ReplyKeyboardMarkup
 
 router = Router()
-router.message.filter(IsPrivateFilter())
+
+token = ""
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    # Создаем кнопку с WebApp
+    button = KeyboardButton(text="Открыть WebApp", web_app=types.WebAppInfo(url="https://d62d-188-19-202-149.ngrok-free.app/login"))
+
+    # Создаем клавиатуру с этой кнопкой
+    keyboard = ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True)
+    await message.answer("Нажмите кнопку ниже для авторизации:", reply_markup=keyboard)
 
 
-@router.message(Command('start'))
-async def start_handler(message: types.Message):
-    await message.answer(f"Hey {message.from_user.first_name}")
+@router.message(filters.WebAppDataFilter())
+async def webapp_data_handler(message: types.Message):
+    # Проверяем, содержит ли сообщение данные WebApp
+    if message.web_app_data:
+        data = message.web_app_data.data
+        await message.answer(f"Получены данные из WebApp: {data}")
+    else:
+        await message.answer("Нет данных от WebApp.")
